130cc1d7cc4a8878688ab5824d42ed4edce28cbe
Yuku Kotani <yukukotani@gmail.com>
2025-01-30 02:05:12 +0900

    run contnet script

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,31 @@
+import { type ContentScriptContext } from "wxt/client";
+
+const watchPattern = new MatchPattern("https://chatgpt.com/c/*");
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ["*://*.chatgpt.com/*"],
+  main(ctx) {
+    console.log("Running content script");
+
+    if (watchPattern.includes(window.location.toString())) {
+      register(ctx);
+    }
+
+    ctx.addEventListener(window, "wxt:locationchange", ({ newUrl }) => {
+      if (watchPattern.includes(newUrl)) {
+        register(ctx);
+      }
+    });
   },
 });
+
+function register(ctx: ContentScriptContext) {
+  console.log("register");
+
+  const [container] = document.getElementsByClassName("@container/thread");
+  if (container == null) {
+    throw new Error("Container element is not found");
+  }
+
+  console.log("Hello content.", container);
+}
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-react"],
+  runner: {
+    startUrls: ["https://chatgpt.com/"],
+    chromiumArgs: ["--user-data-dir=./.wxt/chrome-data"],
+    keepProfileChanges: true,
+  },
 });
